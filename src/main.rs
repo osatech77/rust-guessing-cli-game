9a1a4e1ab1aaea1536810567fd3a4179ab7b4fd8
osatech77 <ozebo87@outlook.com>
2026-01-02 18:43:04 +0000

    Rust is the future, MAANG hire me :)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 fn main() {
-    let correct = rand::rng().random_range(1..=100);
-    println!("Hey guess a number from 1 to 100:");
+    let mut how_many = String::new();
+    println!("How many numbers do you want to generate?");
+    io::stdin()
+        .read_line(&mut how_many)
+        .expect("Failed to read line");
+    let num_guesses: u32 = how_many.trim().parse().expect("Please type a number!");
 
-    loop {
+    let mut correct = Vec::new();
+
+    for _ in 0..num_guesses {
+        correct.push(rand::rng().random_range(1..=100));
+    }
+
+    println!("{:?}", correct); // For debugging purposes
+    let mut guesses_made = 0;
+
+    while guesses_made < num_guesses {
+        println!("Hey guess a number from 1 to 100:");
+
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -15,16 +30,26 @@ fn main() {
             Err(e) => {
                 println!("Please enter a valid number, {e}");
                 continue;
-        }
-    };
+            }
+        };
 
-        match guess.cmp(&correct) {
+        match guess.cmp(&correct[guesses_made as usize]) {
             Ordering::Less => println!("You guessed too low"),
             Ordering::Greater => println!("You guessed too high"),
             Ordering::Equal => {
                 println!("You win!");
-                break;
+                guesses_made += 1;
+                if guesses_made < num_guesses {
+                    println!(
+                        "Now guess the next number! You have guessed {}/{} numbers correctly.",
+                        guesses_made, num_guesses
+                    );
+                }
             }
         };
+    }
+    println!("You guessed all numbers correctly!");
+    for item in correct {
+        println!("The number was: {}", item);
     }
 }
